2c2bdf7e3e2f75d28661da5b40dc5d4547d20a77
NoFoxTuGiv <nofoxtugiv@gmail.com>
2025-02-11 20:57:18 -0500

    Add Head Bob Add Slide Cancels Freelook Tilt to const

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,13 @@
 extends CharacterBody3D
 
 # Player nodes
+@onready var eyes: Node3D = $Neck/Head/Eyes
 @onready var head: Node3D = $Neck/Head
 @onready var neck: Node3D = $Neck
 @onready var standing_col_shape: CollisionShape3D = $standing_col_shape
 @onready var crouching_col_shape: CollisionShape3D = $crouching_col_shape
 @onready var ray_cast_3d: RayCast3D = $RayCast3D
-@onready var camera: Camera3D = $Neck/Head/Camera3D
+@onready var camera: Camera3D = $Neck/Head/Eyes/Camera3D
 
 # Speed Vars
 @export var walk_speed = 5.0
@@ -34,9 +35,22 @@ var direction := Vector3.ZERO
 # Movement Vars
 @export var jump_vel = 4.5
 @export var freelook_enabled = true
-@export var freelook_tilt = -6
+const FREELOOK_TILT = -6
 const lerp_speed = 10.0
 var crouching_depth = -0.5
+
+# Headbob Vars
+@export var bob_enabled = true
+const SPRINT_BOB_SPEED = 22.0
+const WALK_BOB_SPEED = 14.0
+const CROUCH_BOB_SPEED = 10.0
+const CROUCH_BOB_INTENSITY = 0.05
+const SPRINT_BOB_INTENSITY = 0.2
+const WALK_BOB_INTENSITY = 0.1
+
+var bob_vector = Vector2.ZERO
+var bob_index = 0.0
+var current_bob = 0.0
 
 # Functions
 func _ready() -> void:
@@ -64,6 +78,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_vel
+		sliding = false
 
 	# Handle movement states.
 	if Input.is_action_pressed("crouch") and is_on_floor():
@@ -78,7 +93,7 @@ func _physics_process(delta: float) -> void:
 			slide_timer = slide_timer_max
 			slide_vector = input_dir
 			freelooking = true
-			print('Begin slide.')
+			# print('Begin slide.')
 
 		walking = false
 		sprinting = false
@@ -106,7 +121,7 @@ func _physics_process(delta: float) -> void:
 	if  freelook_enabled:
 		if Input.is_action_pressed("freelook") or sliding:
 			freelooking = true
-			camera.rotation.z = deg_to_rad(neck.rotation.y * freelook_tilt)
+			camera.rotation.z = deg_to_rad(neck.rotation.y * FREELOOK_TILT)
 		else:
 			freelooking = false
 			neck.rotation.y = lerp(neck.rotation.y, 0.0, lerp_speed * delta)
@@ -115,11 +130,37 @@ func _physics_process(delta: float) -> void:
 	# Handle slide
 	if sliding:
 		slide_timer -= delta
-		print(slide_timer)
+		# print(slide_timer)
 		if slide_timer < 0:
 			sliding = false
 			freelooking = false
-			print('End Slide')
+			# print('End Slide')
+		elif Input.is_action_just_released("crouch"):
+			sliding = false
+			freelooking = false
+
+	# Handle Headbob
+	match true:
+		sprinting:
+			current_bob = SPRINT_BOB_INTENSITY
+			bob_index += delta * SPRINT_BOB_SPEED
+		walking:
+			current_bob = WALK_BOB_INTENSITY
+			bob_index += delta * WALK_BOB_SPEED
+		crouching:
+			current_bob = CROUCH_BOB_INTENSITY
+			bob_index += delta * CROUCH_BOB_SPEED
+		_:
+			current_bob = 0.0
+	
+	if is_on_floor() and !sliding and input_dir != Vector2.ZERO:
+		bob_vector.y = sin(bob_index)
+		bob_vector.x = sin(bob_index/2) * 0.5
+		eyes.position.y = lerp(eyes.position.y, bob_vector.y * (current_bob / 2), delta * lerp_speed)
+		eyes.position.x = lerp(eyes.position.x, bob_vector.x * current_bob, delta * lerp_speed)
+	else:
+		eyes.position.y = lerp(eyes.position.y, 0.0, delta * lerp_speed)
+		eyes.position.x = lerp(eyes.position.x, 0.0, delta * lerp_speed)
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
